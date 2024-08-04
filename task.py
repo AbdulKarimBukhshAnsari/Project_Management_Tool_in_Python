1de7c9ccca73e0812c67feb6865a925f138123e9
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-08-04 16:28:46 +0500

    started task class and date class

---
+++ task.py
@@ -0,0 +1,22 @@
+# this class is made to handle the all things which is related to task 
+
+
+class Task:
+    def __init__(self,task_id,name,description,starting_date,ending_date,priority,status) -> None:
+        self.task_id = task_id
+        self.name = name
+        self.desciption = description
+        self.starting_date = starting_date
+        self.ending_date = ending_date
+        self.priority = priority
+        self.status = status 
+    def add_task():
+        pass
+
+    def update_task():
+        pass
+
+    def delete_task():
+        pass
+    
+     
